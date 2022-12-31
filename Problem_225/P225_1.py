0e8415fd4363d558e01fc050a9597fe746d9248a
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-12-31 09:34:50 +0530

    P225 2

--- Problem_225/P225_1.py
+++ Problem_225/P225_1.py
@@ -12,7 +12,21 @@ def tribonacci(t_seq):
 
 
 if __name__ == '__main__':
-    T = [1, 1, 1]
-    for i in range(int(1e04)):
-        T = tribonacci(T)
-    print(T)
+    trib_seq = [1, 1, 1]
+    for i in range(int(16225)):
+        # print(i)
+        trib_seq = tribonacci(trib_seq)
+    print(trib_seq)
+
+    non_div = []
+    for odd_num in range(3, int(5e03), 2):
+        FLAG = True
+        for trib in trib_seq:
+            if trib % odd_num == 0:
+                FLAG = False
+                break
+        if FLAG:
+            # print(odd_num)
+            non_div.append(odd_num)
+    print(non_div)
+    print("124th odd number is", non_div[124 - 1])
