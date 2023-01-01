3cca1adae9ee8d317f91d6ea6bf6caabe6d4cd59
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-01 09:42:04 +0530

    P725 4

--- Problem-725/P725_2.py
+++ Problem-725/P725_2.py
@@ -4,29 +4,34 @@ P725_2
 import itertools
 
 
-def partitions(n, I=1):
+def partitions(num, init=1):
     """
     https://stackoverflow.com/a/44209393
     """
-    yield (n,)
-    for i in range(I, n//2 + 1):
-        for p in partitions(n-i, i):
-            yield (i,) + p
+    yield (num,)
+    for i in range(init, num//2 + 1):
+        for part in partitions(num-i, i):
+            yield (i,) + part
 
 
-def ds_n(N):
+def ds_n(num_digits, num_non_zero = -1):
     """
-    Sum of all DS-numbers of (exactly) N digits.
+    Sum of all DS-numbers of N digits.
+    num_digits: Total number of digits.
+    num_non_zero: Number of non-zero digits.
     """
+    if num_non_zero == -1:
+        num_non_zero = num_digits
+
     sum_ds = 0
-    for d in range(10):
-        for p in partitions(d):
-            if len(p) == (N-1):
-                perms = itertools.permutations([*p, d])
+    for digit in range(10):
+        for part in partitions(digit):
+            if len(part) == (num_non_zero-1):
+                perms = itertools.permutations([*part, digit])
                 uniq_perms = set(perms)
-                print(d, uniq_perms)
-                sum_ds += (2*d) * len(uniq_perms)
-    return N, sum_ds
+                print(digit, uniq_perms)
+                sum_ds += (2*digit) * len(uniq_perms)
+    return num_digits, sum_ds
 
 
 if __name__ == '__main__':
