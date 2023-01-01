a05280a60657019e431cd0fd080fe7fdcb37428a
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-01 16:57:51 +0530

    P725 9

--- Problem-725/P725_3.py
+++ Problem-725/P725_3.py
@@ -2,6 +2,7 @@
 P725_3
 """
 import itertools
+from collections import Counter
 
 
 def partitions(num, init=1):
@@ -12,6 +13,24 @@ def partitions(num, init=1):
     for anchor in range(init, num//2 + 1):
         for partition in partitions(num-anchor, anchor):
             yield (anchor,) + partition
+
+
+def diff(first, second):
+    """
+    https://stackoverflow.com/a/63319667
+    """
+    second_cntr = Counter(second)
+    second = set(second)
+    res = []
+    for i in first:
+        if i not in second:
+            res.append(i)
+        elif i in second_cntr:
+            if second_cntr[i] > 0:
+                second_cntr[i] -= 1
+            else:
+                res.append(i)
+    return res
 
 
 if __name__ == '__main__':
@@ -25,19 +44,26 @@ if __name__ == '__main__':
             print(part)
             for choice in range(1, len(part)+1):
                 print(choice)
+                combs = itertools.combinations(part, choice)
+                uniq_combs = set(combs)
+
                 for sub_part in itertools.combinations(part, choice):
                     print(sub_part)
-                    rest_part = list(set(part) - set(sub_part))
+                    rest_part = diff(part, sub_part)
                     print(rest_part)
 
-                    zeros = NUM_DIGITS - MOD - len(rest_part)
-                    perms = itertools.permutations([*[0]*zeros, *rest_part])
+                    ZEROS = NUM_DIGITS - MOD - len(rest_part)
+                    print(ZEROS)
+                    perms = itertools.permutations([*[0]*ZEROS, *rest_part])
+                    print(perms)
                     uniq_perms = set(perms)
                     print(uniq_perms)
                     count = len(uniq_perms)
 
-                    zeros = MOD - len(sub_part)
-                    perms = itertools.permutations([*[0]*zeros, *sub_part])
+                    ZEROS = MOD - len(sub_part)
+                    print(ZEROS)
+                    perms = itertools.permutations([*[0]*ZEROS, *sub_part])
+                    print(perms)
                     uniq_perms = set(perms)
                     print(uniq_perms)
                     for perm in uniq_perms:
