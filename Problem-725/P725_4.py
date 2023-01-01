8f1df81b69de77196bea1f5d736de3e21278f14f
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-02 01:46:15 +0530

    P725 15

--- Problem-725/P725_4.py
+++ Problem-725/P725_4.py
@@ -49,23 +49,19 @@ if __name__ == '__main__':
     for digit in range(1, 10):
         for part in partitions(digit):
             part = [*part, digit]
-            # print(part)
             for choice in range(1, len(part)+1):
-                # print(choice)
                 combs = itertools.combinations(part, choice)
                 uniq_combs = set(combs)
 
-                for sub_part in itertools.combinations(part, choice):
+                for sub_part in uniq_combs:
                     rest_part = diff(part, sub_part)
                     sub_part_counter = Counter(sub_part)
                     rest_part_counter = Counter(rest_part)
                     uniq_sub_part = set(sub_part)
                     uniq_rest_part = set(rest_part)
-                    print(rest_part, sub_part)
 
                     TERM_1 = math.comb(MOD-1, len(sub_part))
                     TERM_2 = math.comb(NUM_DIGITS-MOD, len(rest_part))
-                    print(TERM_1, TERM_2)
 
                     TERM_3 = 1
                     for index in uniq_sub_part:
@@ -78,13 +74,14 @@ if __name__ == '__main__':
                         TERM_4 *= math.factorial(count)
 
                     PROD = 1
-                    PROD_1 = TERM_1 / TERM_3
-                    PROD_2 = TERM_2 / TERM_4
+                    PROD_1 = TERM_1 // TERM_3
+                    PROD_2 = TERM_2 // TERM_4
                     PROD *= with_mod(TERM_1, MOD)
                     PROD *= with_mod(TERM_2, MOD)
-                    PROD *= 2*digit
-                    PROD *= with_mod(PROD, MOD)
-                    print(PROD)
+                    PROD *= sum(sub_part)
+                    PROD = with_mod(PROD, MOD)
+                    print(digit, part, rest_part, sub_part, TERM_1, TERM_2,
+                        TERM_3, TERM_4, PROD_1 ,PROD_2, sum(sub_part), PROD)
                     SUM += PROD
 
     NUM = int("1"*MOD)
